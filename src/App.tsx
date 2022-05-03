c70c066d3f2c96d6d48318e4e00f54be4eafad1d
Nazar Pechevystyi <pechevystyi@vialutions.com>
2022-05-03 23:52:42 +0200

    added sorting and icon

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ interface Video {
   description: string;
   link: string;
   chapters: Chapter[];
+  score: number;
 }
 
 interface Chapter {
@@ -72,18 +73,34 @@ const App = () => {
             keys: ["title"],
             threshold: 0.3,
             ignoreLocation: true,
+            includeScore: true,
           });
 
-          const fuseMatches = fuse.search(query).map((match) => match.item);
+          const fuseMatches = fuse
+            .search(query)
+            .map((match) => ({ ...match.item, score: match.score }));
           const literalMatches = vid.chapters.filter(
             (chapter) =>
               !fuseMatches.some((match: any) => match.time === chapter.time) &&
               chapter.title.toLowerCase().includes(query.toLowerCase())
           );
+          let maxMatch = 0;
+          if (literalMatches.length > 0) {
+            maxMatch = 1;
+          } else {
+            maxMatch = Math.max(
+              ...fuseMatches.map((match: any) => match!.score)
+            );
+          }
           const matches = literalMatches.concat(fuseMatches);
           if (matches.length) {
-            vids.push({ ...vid, chapters: matches });
+            vids.push({
+              ...vid,
+              chapters: matches,
+              score: maxMatch,
+            });
           }
+          vids.sort((a: Video, b: Video) => b.score - a.score);
           return vids;
         },
         []
@@ -107,15 +124,23 @@ const App = () => {
             />{" "}
             Право на Поплаву
           </Navbar.Brand>
-          <Button variant="primary">
-            <a
+          <div>
+            <Button
+              variant="danger"
+              href="https://www.youtube.com/channel/UCwCkRo2WQx_9JRWISLC47fw"
+              target="_blank"
+            >
+              YouTube
+            </Button>
+            <Button
+              variant="primary"
               href="https://savelife.in.ua/donate/"
               target="_blank"
-              className="text-white text-decoration-none"
+              className="ms-4"
             >
               Донат на ЗСУ
-            </a>
-          </Button>
+            </Button>
+          </div>
         </Container>
       </Navbar>
       <Container>
@@ -130,46 +155,46 @@ const App = () => {
           />
         </div>
 
-        <Accordion>
-          {filteredVideos?.map((video: Video) => {
-            if (video) {
-              return (
-                <Accordion.Item eventKey={video.id} key={video.id}>
-                  <Accordion.Header>
-                    <div className="d-flex justify-content-between w-100">
-                      {video.title}
-                      <a href={video.link} target="_blank">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          fill="red"
-                          className="bi bi-youtube me-3"
-                          viewBox="0 0 16 16"
-                          onClick={(e) => e.stopPropagation()}
-                        >
-                          <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
-                        </svg>
-                      </a>
-                    </div>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    {video.chapters?.map((chapter) => (
-                      <a
-                        key={chapter.link}
-                        className="d-block"
-                        href={chapter.link}
-                        target="_blank"
+        {filteredVideos.length ? (
+          <Accordion>
+            {filteredVideos?.map((video: Video) => (
+              <Accordion.Item eventKey={video.id} key={video.id}>
+                <Accordion.Header>
+                  <div className="d-flex justify-content-between w-100">
+                    {video.title}
+                    <a href={video.link} target="_blank">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="red"
+                        className="bi bi-youtube me-3"
+                        viewBox="0 0 16 16"
+                        onClick={(e) => e.stopPropagation()}
                       >
-                        {chapter.time + " " + chapter.title}
-                      </a>
-                    ))}
-                  </Accordion.Body>
-                </Accordion.Item>
-              );
-            }
-          })}
-        </Accordion>
+                        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
+                      </svg>
+                    </a>
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                  {video.chapters?.map((chapter) => (
+                    <a
+                      key={chapter.link}
+                      className="d-block"
+                      href={chapter.link}
+                      target="_blank"
+                    >
+                      {chapter.time + " " + chapter.title}
+                    </a>
+                  ))}
+                </Accordion.Body>
+              </Accordion.Item>
+            ))}
+          </Accordion>
+        ) : (
+          <h3>Нічого не знайдено, спробуйте шукати по ключових словах ;)</h3>
+        )}
       </Container>
     </>
   );
